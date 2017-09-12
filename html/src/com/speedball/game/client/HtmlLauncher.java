4d2fae9a0e72b15fdeaf3d16e565bd5f8cc393d7
brenner0111 <brennerharris7@yahoo.com>
2017-09-11 22:11:19 -0400

    inital commit with code

---
+++ html/src/com/speedball/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.speedball.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.speedball.game.SpeedBall;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SpeedBall();
+        }
+}
