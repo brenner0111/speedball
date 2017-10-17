a352ae4096895fd7930f51530e49f3a694bde747
Calvin Yarboro <yarborocl@appstate.edu>
2017-10-16 21:50:33 -0400

    fixed duplicate code in Utils

--- core/src/com/speedball/game/Utils.java
+++ core/src/com/speedball/game/Utils.java
@@ -166,20 +166,4 @@ public class Utils {
 		}
 		return vertices;
 	}
-	protected float[] convertArrayList(ArrayList<Float> vertices) { 
-        float[] floatArray = new float[vertices.size()];
-        int i = 0;
-
-        for (Float f : vertices) {
-            floatArray[i++] = (f != null ? f : Float.NaN);
-        }
-        return floatArray;
-    }
-    protected ArrayList<Float> convertArray(float[] array) { 
-        ArrayList<Float> vertices = new ArrayList<Float>();
-        for (int i = 0; i < array.length; i++) {
-            vertices.add(array[i]);
-        }
-        return vertices;
-    }
 }
