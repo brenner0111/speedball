acd3afc8a54f137b8185ac67e23706202d7b2f79
brenner0111 <brennerharris7@yahoo.com>
2017-10-23 21:23:04 -0400

    Fixed paintball collisions with bunkers wooo!

--- core/src/com/speedball/game/GunUtils.java
+++ core/src/com/speedball/game/GunUtils.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,175 +17,186 @@ import com.badlogic.gdx.math.Vector3;
 
 public class GunUtils {
     
-    private static final float PBALL_DIST_CAP = 25.0f;
+    private static final float PBALL_DIST_CAP = 1.0f;
 
-	protected Paintball createPaintballSprite(float x, float y, float slope, int quadrant) {
-	    FileHandle paintBallHandle = Gdx.files.internal("paintballs/redPaintball.png");
-	    Texture backgroundTexture = new Texture(paintBallHandle);
-	    return new Paintball(backgroundTexture, slope, quadrant);
-	    
-	}
-	protected float getPlayerGunCoord(float coordinate, float offSet, boolean isX) {
-	    //return coordinate of the tip of the gun barrel
-	    if (isX) {
-	        return coordinate + offSet;
-	    }
-	    else {
-	        return coordinate + offSet;
-	    }
-	}
-	
-	protected int drawPaintballs(SpriteBatch b, ArrayList<Paintball> paintballs, int paintballCounter, ArrayList<Bunker> bunkers) {
-		for (Iterator<Paintball> iterator = paintballs.iterator(); iterator.hasNext();) {
-			Paintball paintball = iterator.next();
-			if (paintball.getCollided() == false) {
-				updatePaintballXY(paintball, paintball.getQuadrant());
-			}
-			//System.out.println(Arrays.toString(getPaintballVertices(paintball)));
-			paintballCollided(paintball, bunkers);
-			if (paintballInWindow(paintball)) {
-				paintball.draw(b);
-			}
-			else {
-				iterator.remove();
-				paintballCounter = paintballCounter - 1;
-			}
-		}
-		return paintballCounter;
-	}
-	/**
-	 * OPTODO: add paintball slowdown overtime
-	 * @param paintball
-	 * @param paintballSpeed
-	 */
-	protected void updatePaintballXY(Paintball paintball, int quadrant) {
-		float slope = Math.abs(paintball.getSlope());
-		float r = (float)Math.sqrt(1 + Math.pow(slope, 2));
-		float x = 0.0f;
-		float y = 0.0f;
-		
-		if (quadrant == 1) {
-		    x = paintball.getX() + (PBALL_DIST_CAP/r);
-		    y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
-		    paintball.setX(x);
-            paintball.setY(y);
-		}
-		else if (quadrant == 2) {
-		    x = paintball.getX() - (PBALL_DIST_CAP/r);
-		    y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
-		    paintball.setX(x);
-            paintball.setY(y);
-		}
-		else if (quadrant == 3) {
-		    x = paintball.getX() - (PBALL_DIST_CAP/r);
-	        y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
-		    paintball.setX(x);
-            paintball.setY(y);
-		}
-		else if (quadrant == 4){
-		    x = paintball.getX() + (PBALL_DIST_CAP/r);
-		    y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
-		    paintball.setX(x);
-            paintball.setY(y);
-		}
-		else if (quadrant == 0) {
-			x = paintball.getX() + (PBALL_DIST_CAP/r);
-			paintball.setX(x);
-		}
-		else if (quadrant == 90) {
-			y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
-			paintball.setY(y);
-		}
-		else if (quadrant == 180) {
-			x = paintball.getX() - (PBALL_DIST_CAP/r);
-			paintball.setX(x);
-		}
-		else if (quadrant == 270) {
-			 y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
-			 paintball.setY(y);
-		}
-		else {
-			
-		}
-	}
+    protected Paintball createPaintballSprite(float x, float y, float slope, int quadrant) {
+        FileHandle paintBallHandle = Gdx.files.internal("paintballs/redPaintball.png");
+        Texture backgroundTexture = new Texture(paintBallHandle);
+        return new Paintball(backgroundTexture, slope, quadrant);
+        
+    }
+    protected float getPlayerGunCoord(float coordinate, float offSet, boolean isX) {
+        //return coordinate of the tip of the gun barrel
+        if (isX) {
+            return coordinate + offSet;
+        }
+        else {
+            return coordinate + offSet;
+        }
+    }
+    
+    protected int drawPaintballs(SpriteBatch b, ArrayList<Paintball> paintballs, int paintballCounter, ArrayList<Bunker> bunkers) {
+        for (Iterator<Paintball> iterator = paintballs.iterator(); iterator.hasNext();) {
+            Paintball paintball = iterator.next();
+            
+            if (!paintball.getCollided()) {
+                paintballCollided(paintball, bunkers, paintball.getQuadrant());
+            }
+    
+            if (paintballInWindow(paintball)) {
+                paintball.draw(b);
+            }
+            else {
+                iterator.remove();
+                paintballCounter = paintballCounter - 1;
+            }
+        }
+        return paintballCounter;
+    }
 
-	protected int checkQuadrant(float angle) {
-		if (angle > 0.0f && angle < 90.0f) {
-			return 1;
-		}
-		else if (angle > 90.f && angle < 180.0f) {
-			return 2;
-		}
-		else if (angle > 180.0f && angle < 270.0f) {
-			return 3;
-		}
-		else if (angle > 270.0f && angle < 360.0f) {
-			return 4;
-		}
-		else if (angle == 0.0f || angle == 360.0f) {
-			return 0;
-		}
-		else if (angle == 90.0f) {
-			return 90;
-		}
-		else if (angle == 180.0f) {
-			return 180;
-		}
-		else if (angle == 270.0f) {
-			return 270;
-		}
-		else {
-			return -1;
-		}
-		
-	}
-	
-	protected float[] updateRealGunXY(float angle, float centerX, float centerY, float gunRadius) {
-	    float[] coords = new float[2];
-	    coords[0] = centerX + (float)(gunRadius * Math.cos(Math.toRadians(angle)));
+    /**
+     * OPTODO: add paintball slowdown overtime
+     * @param paintball
+     * @param paintballSpeed
+     */
+    protected void updatePaintballXY(Paintball paintball, int quadrant) {
+        float slope = Math.abs(paintball.getSlope());
+        float r = (float)Math.sqrt(1 + Math.pow(slope, 2));
+        float x = 0.0f;
+        float y = 0.0f;
+        
+        if (quadrant == 1) {
+            x = paintball.getX() + (PBALL_DIST_CAP/r);
+            y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
+            paintball.setX(x);
+            paintball.setY(y);
+        }
+        else if (quadrant == 2) {
+            x = paintball.getX() - (PBALL_DIST_CAP/r);
+            y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
+            paintball.setX(x);
+            paintball.setY(y);
+        }
+        else if (quadrant == 3) {
+            x = paintball.getX() - (PBALL_DIST_CAP/r);
+            y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
+            paintball.setX(x);
+            paintball.setY(y);
+        }
+        else if (quadrant == 4){
+            x = paintball.getX() + (PBALL_DIST_CAP/r);
+            y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
+            paintball.setX(x);
+            paintball.setY(y);
+        }
+        else if (quadrant == 0) {
+            x = paintball.getX() + (PBALL_DIST_CAP/r);
+            paintball.setX(x);
+        }
+        else if (quadrant == 90) {
+            y = paintball.getY() + ((PBALL_DIST_CAP*slope)/r);
+            paintball.setY(y);
+        }
+        else if (quadrant == 180) {
+            x = paintball.getX() - (PBALL_DIST_CAP/r);
+            paintball.setX(x);
+        }
+        else if (quadrant == 270) {
+             y = paintball.getY() - ((PBALL_DIST_CAP*slope)/r);
+             paintball.setY(y);
+        }
+        System.out.println("Paintball X: " + paintball.getX() + " Paintball Y: " + paintball.getY());
+    }
+
+    protected int checkQuadrant(float angle) {
+        if (angle > 0.0f && angle < 90.0f) {
+            return 1;
+        }
+        else if (angle > 90.f && angle < 180.0f) {
+            return 2;
+        }
+        else if (angle > 180.0f && angle < 270.0f) {
+            return 3;
+        }
+        else if (angle > 270.0f && angle < 360.0f) {
+            return 4;
+        }
+        else if (angle == 0.0f || angle == 360.0f) {
+            return 0;
+        }
+        else if (angle == 90.0f) {
+            return 90;
+        }
+        else if (angle == 180.0f) {
+            return 180;
+        }
+        else if (angle == 270.0f) {
+            return 270;
+        }
+        else {
+            return -1;
+        }
+        
+    }
+    
+    protected float[] updateRealGunXY(float angle, float centerX, float centerY, float gunRadius) {
+        float[] coords = new float[2];
+        coords[0] = centerX + (float)(gunRadius * Math.cos(Math.toRadians(angle)));
         coords[1] = centerY + (float)(gunRadius * Math.sin(Math.toRadians(angle)));
-	    return coords;
-	}
+        return coords;
+    }
 
-	protected float[] getPaintballVertices(Paintball paintball) {
-		Rectangle rectangle = paintball.getBoundingRectangle();
-		float width = rectangle.getWidth();
-		float height = rectangle.getHeight();
-		float x = rectangle.getX();
-		float y = rectangle.getY();
-		float[] retArray = {x, y, x + width, y, x + width, y + height, x, y + height};
-		return retArray;
-	}
-	protected boolean paintballInWindow(Paintball paintball) {
-		float x = paintball.getBoundingRectangle().getX();
-		float y = paintball.getBoundingRectangle().getY();
-		if (x > 1045 || x < 0) {
-			return false;
-		}
-		else if (y > 690 || y < 0) {
-			return false;
-		}
-		return true;
-	}
-	protected void paintballCollided(Paintball paintball, ArrayList<Bunker> bunkers) {
-		float[] paintballVertices = getPaintballVertices(paintball);
-		for (Bunker bunker: bunkers) {
-			if (bunker.isCollidable()) {
-				float[] bunkerVertices = bunker.getVerticesArray();
-				if (bunkerVertices.length != 0 && Intersector.overlapConvexPolygons(paintballVertices, bunkerVertices, null)) {
-					paintball.setCollided(true);
-					break;
-				}
-				else {
-					paintball.setCollided(false);
-				}
-			}
-		}
-	}
-	protected boolean checkAndFireGun(float angle, Player player, Camera camera) {
-		// If screen was just touched or left click was pushed on desktop
-	    // Gdx.input.isButtonPressed(Input.Buttons.LEFT)
-		if (Gdx.input.justTouched()) {
+    protected float[] getPaintballVertices(Paintball paintball) {
+        Rectangle rectangle = paintball.getBoundingRectangle();
+        float width = rectangle.getWidth();
+        float height = rectangle.getHeight();
+        float x = rectangle.getX();
+        float y = rectangle.getY();
+        float[] retArray = {x, y, x + width, y, x + width, y + height, x, y + height};
+        return retArray;
+    }
+    
+    protected boolean paintballInWindow(Paintball paintball) {
+        float x = paintball.getBoundingRectangle().getX();
+        float y = paintball.getBoundingRectangle().getY();
+        if (x > 1045 || x < 0) {
+            return false;
+        }
+        else if (y > 690 || y < 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    protected boolean paintballCollided(Paintball paintball, ArrayList<Bunker> bunkers, int quadrant) {
+        float[] paintballVertices = getPaintballVertices(paintball);
+        
+        for (int i = 0; i < 25; i++) {
+            
+            for (Bunker bunker: bunkers) {
+                if (bunker.isCollidable()) {
+                    float[] bunkerVertices = bunker.getVerticesArray();
+                    if (bunkerVertices.length != 0 && Intersector.overlapConvexPolygons(paintballVertices, bunkerVertices, null)) {
+                        paintball.setCollided(true);
+                        return true;
+                    }
+                    else {
+                        paintball.setCollided(false);
+                    }
+                }
+            }
+            updatePaintballXY(paintball, quadrant);
+            paintballVertices = getPaintballVertices(paintball);
+        } 
+        return false; 
+    }
+    
+    
+    protected boolean checkAndFireGun(float angle, Player player, Camera camera) {
+        // If screen was just touched or left click was pushed on desktop
+        // Gdx.input.isButtonPressed(Input.Buttons.LEFT)
+        //Gdx.input.justTouched()
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
             //sets gun position before rotation
             player.setGunX(getPlayerGunCoord(player.getPlayerX(), Player.getPlayerGunWidth(), true)); 
             player.setGunY(getPlayerGunCoord(player.getPlayerY(), Player.getPlayerGunHeight(), false));
@@ -204,9 +216,9 @@ public class GunUtils {
             //Add the paintball into the ArrayList with it's current state
             player.addPaintball(createPaintballSprite(player.getGunX(), player.getGunY(), slope, quadrant));
 
-            System.out.println("ANGLE: " + angle + " SLOPE: " + slope + " realgunX: " + coords[0] + " realgunY: " + coords[1] + 
+            /*System.out.println("ANGLE: " + angle + " SLOPE: " + slope + " realgunX: " + coords[0] + " realgunY: " + coords[1] + 
                 " fakeGunX: "+ player.getGunX() + " fakeGunY: " + player.getGunY() + " playerX: " + player.getPlayerX() + " playerY:" + player.getPlayerY() + " Quadrant: " + quadrant +
-                " MouseXY: (" + Gdx.input.getX() + ", " + Gdx.input.getY() + ")" + "MappedMouseXY: (" + tmpCoords.x + "f, " + tmpCoords.y + "f,)" );
+                " MouseXY: (" + Gdx.input.getX() + ", " + Gdx.input.getY() + ")" + "MappedMouseXY: (" + tmpCoords.x + "f, " + tmpCoords.y + "f,)" );*/
             player.incrementPaintballCounter();
             return true;
         }
